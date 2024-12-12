ac966f54e96809c4ada367d097f10df41013c81b
Bing Yan <32602403+bingyan4science@users.noreply.github.com>
2024-12-12 07:28:34 -0500

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,12 @@ def get_sep_position(input_ids, sep_id):
     batch_size = input_ids.shape[0]
     sep_positions = input_ids.new_zeros(batch_size).long()
     for batch_id in range(batch_size):
-        sep_position = input_ids[batch_id].eq(sep_id).nonzero()[0, -1].item()
+        #import ipdb; ipdb.set_trace()
+        nonzero = len(input_ids[batch_id].eq(sep_id).nonzero())
+        if nonzero > 0:
+            sep_position = input_ids[batch_id].eq(sep_id).nonzero()[0, -1].item()
+        else:
+            sep_position = len(input_ids[batch_id])-1
         sep_positions[batch_id] = sep_position
     return sep_positions
 
