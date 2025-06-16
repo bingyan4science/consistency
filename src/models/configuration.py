b65e2515aad882fc787aa62fd9a1f1e52fd9178e
bingyan4science <by2192@log-2.hpc.nyu.edu>
2025-06-16 16:24:58 -0400

    update 0616

--- src/models/configuration.py
+++ src/models/configuration.py
@@ -4,11 +4,12 @@ class Config(PretrainedConfig):
     def __init__(
         self,
         base_model='gpt2',
-        tokenizer_name='gpt2',
+        #tokenizer_name='gpt2',
+        tokenizer_name=None,
         **kwargs,
     ):
         self.base_model = base_model
-        self.tokenizer_name = tokenizer_name
+        self.tokenizer_name = tokenizer_name if tokenizer_name is not None else base_model
         super().__init__(**kwargs)
 
 
